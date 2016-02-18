7dc8522e759ffac17c39d1597aba4698d1da2a9b
Derek Sims <derekholio1@gmail.com>
2016-02-18 00:47:20 -0600

    Added Tasks table specs

--- database/migrations/2016_02_16_012156_create_tasks_table.php
+++ database/migrations/2016_02_16_012156_create_tasks_table.php
@@ -12,6 +12,16 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
+        /*
+         * Tasks specifications:
+         * Title (name)
+         * Description
+         * Assigned user (FK to users table)
+         * Due date
+         * Category (FK to category table)
+         * Color
+         * Discussion thread
+         */
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
