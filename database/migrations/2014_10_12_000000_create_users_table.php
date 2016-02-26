7b014d0938c04ff3f614daa9e78ce92a28c6a9fa
Derek Sims <dsims@unomaha.edu>
2016-02-25 19:27:03 -0500

    Revert "Revert back to good commit"
    This reverts commit 6588a30d7e8166497d240e154d2d09ca8b558245.
    
    Revert last commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
+	    $table->string('first_name',40);
+	    $table->string('last_name', 40);
             $table->string('password', 60);
             $table->rememberToken();
             $table->timestamps();
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
     }
+}
+
+class UserTableSeeder extends Seeder {
+
+public function run(){
+
+$ironman = DB::table('users')->insert ([
+	'username' => 'ironman',
+	'email'    => 'iam@ironman.com',
+	'password' => Hash::make('ironman'),
+	'first_name' => 'Tony',
+	'last_name' => 'Stark',
+	'created_at' => new DateTime(),
+	'updated_at' => new DateTime()
+]);
+
+$Peyton = DB::table('users')->insert ([
+	'username' => 'champ2016',
+	'email'    => 'peyton@sbchamp2016.com',
+	'password' => Hash::make('ironman'),
+	'first_name' => 'Peyton',
+	'last_name' => 'Manning',
+	'created_at' => new DateTime(),
+	'updated_at' => new DateTime()
+]);
+
+}
+
 }
