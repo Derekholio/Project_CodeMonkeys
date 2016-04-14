24cf5c790028f99eb8226a2f0e6f0702c6fdfca7
= <=>
2016-04-14 17:52:49 -0400

    Added description and priority

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -24,8 +24,27 @@
 								<input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
 							</div>
 						</div>
+						<div class="form-group">
+							<label for="task-description" class="col-sm-3 control-label">Description</label>
 
-						<!-- Add Task Button -->
+							<div class="col-sm-6">
+								<input type="text" name="description" id="task-description" class="form-control" value="{{ old('task') }}">
+							</div>
+							</div>
+
+
+
+
+
+<div class="form-group">
+<label for="task-priority" class="col-sm-3 control-label">Priority</label>
+<label class="radio-inline"><input type="radio" name="priority" value="3" id="task-priority">Low</label>
+<label class="radio-inline"><input type="radio" name="priority" value="2" id="task-priority">Medium</label>
+<label class="radio-inline"><input type="radio" name="priority" value="1" id="task-priority">High</label>
+
+
+
+</div>						<!-- Add Task Button -->
 						<div class="form-group">
 							<div class="col-sm-offset-3 col-sm-6">
 								<button type="submit" class="btn btn-default">
