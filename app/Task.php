19d6b6ccf08acd4690181cb114b410b76934aebc
Derek Sims <derekholio1@gmail.com>
2016-04-17 14:12:57 -0500

    Added a priority relation to the Task model (fulfills the foreign key of tasks.priority_id to priority.id)

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    public function priority(){
+        return $this->hasOne('App\Priority', 'id', 'priority_id');
+    }
 }
