a3d12000608d461eb4ce04e299802712effcbb62
Derek Sims <support@derekholio.com>
2016-04-18 23:46:50 -0500

    Added the Task/User relationship

--- app/Task.php
+++ app/Task.php
@@ -9,4 +9,8 @@ class Task extends Model
     public function priority(){
         return $this->hasOne('App\Priority', 'id', 'priority_id');
     }
+
+    public function user(){
+        return $this->hasOne('App\User', 'id', 'assignee_id');
+    }
 }
