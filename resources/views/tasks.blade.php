9c7b631651984979fa67924e7d0784b6de34d478
Jared Knust <jared.knust@gmail.com>
2016-04-20 11:42:59 -0500

    New Task List Category

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -86,5 +86,50 @@
 				</div>
 			@endif
 		</div>
+
+<!-- Completed -->
+
+		<div class="col-sm-offset-2 col-lg-3">
+			<!-- Current Tasks -->
+			@if (count($tasks) > 0)
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						Completed
+					</div>
+
+					<div class="panel-body">
+						<table class="table table-striped task-table">
+							<thead>
+							<th>Task</th>
+							<th>&nbsp;</th>
+							</thead>
+							<tbody>
+							@foreach ($tasks as $task)
+								<tr>
+									<td class="table-text">
+										<div>
+											<a href="/task/{{$task->id}}">{{ $task->name }}</a>
+										</div>
+									</td>
+									<!-- Task Delete Button -->
+									<td>
+										<form action="/task/{{ $task->id }}" method="POST">
+											{{ csrf_field() }}
+											{{ method_field('DELETE') }}
+
+											<button type="submit" class="btn btn-danger">
+												<i class="fa fa-trash"></i>Delete
+											</button>
+										</form>
+									</td>
+								</tr>
+							@endforeach
+							</tbody>
+						</table>
+					</div>
+				</div>
+			@endif
+		</div>
+
 	</div>
 @endsection
