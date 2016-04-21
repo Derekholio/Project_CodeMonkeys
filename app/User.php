3b25ca1acd8765a18476a20b84d5a63f88d480d2
Kyle Leonard <kleonard@unomaha.edu>
2016-04-21 08:57:57 -0500

    Fixes name entry.

--- app/User.php
+++ app/User.php
@@ -25,7 +25,7 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-    protected $fillable = ['first_name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password'];
     /**
      * The attributes excluded from the model's JSON form.
      *
