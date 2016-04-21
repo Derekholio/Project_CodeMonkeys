0cc667e8e9b1cafe89f3c720351618b7939cfafe
Derek Sims <support@derekholio.com>
2016-04-20 19:03:04 -0500

    Removed un-needed spacing in tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -3,43 +3,43 @@
 @section('content')
 	<div align="center" class="container-fluid fullscreen centered" >
 		<div class="col-lg-3">
-						<!-- Current Tasks -->
-						@if (count($tasks) > 0)
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									Current Tasks
-								</div>
+			<!-- Current Tasks -->
+			@if (count($tasks) > 0)
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						Current Tasks
+					</div>
 
-								<div class="panel-body">
-									<table class="table table-striped task-table">
-										<thead>
-										<th>Task</th>
-										<th>&nbsp;</th>
-										</thead>
-										<tbody>
-										@foreach ($tasks as $task)
-											<tr>
-												<td class="table-text">
-													<div>
-														<a href="/task/{{$task->id}}">{{ $task->name }}</a>
-													</div>
-												</td>
-												<!-- Task Delete Button -->
-												<td>
-													<form action="/task/{{ $task->id }}" method="POST">
-														{{ csrf_field() }}
-														{{ method_field('DELETE') }}
+					<div class="panel-body">
+						<table class="table table-striped task-table">
+							<thead>
+							<th>Task</th>
+							<th>&nbsp;</th>
+							</thead>
+							<tbody>
+							@foreach ($tasks as $task)
+								<tr>
+									<td class="table-text">
+										<div>
+											<a href="/task/{{$task->id}}">{{ $task->name }}</a>
+										</div>
+									</td>
+									<!-- Task Delete Button -->
+									<td>
+										<form action="/task/{{ $task->id }}" method="POST">
+											{{ csrf_field() }}
+											{{ method_field('DELETE') }}
 
-														<button type="submit" class="btn btn-danger">
-															<i class="fa fa-trash"></i>Delete
-														</button>
-													</form>
-												</td>
-											</tr>
-										@endforeach
-										</tbody>
-									</table>
-								</div>
+											<button type="submit" class="btn btn-danger">
+												<i class="fa fa-trash"></i>Delete
+											</button>
+										</form>
+									</td>
+								</tr>
+							@endforeach
+							</tbody>
+						</table>
+					</div>
 				</div>
 			@endif
 		</div>
