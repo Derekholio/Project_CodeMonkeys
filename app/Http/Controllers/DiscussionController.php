e75c6ba363dd614fdd7a5dbd33f07cb364782136
Derek Sims <support@derekholio.com>
2016-04-21 11:21:59 -0500

    Cleaned up routes.php and moved all of the application logic into controllers

---
+++ app/Http/Controllers/DiscussionController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Discussions;
+use Auth;
+use Validator;
+
+class DiscussionController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function addChatMessage(Request $request, $id){
+        if(Auth::check()){
+            $validator = Validator::make($request->all(),[
+                'discussion' => 'required|min:1',
+            ]);
+
+            if ($validator->fails()) {
+                return redirect("/task/$id")
+                    ->withInput()
+                    ->withErrors($validator);
+            }
+
+            $discussion = new Discussions;
+            $discussion->message = $request->discussion;
+            $discussion->posted_time = Carbon\Carbon::now();
+            $discussion->task_id = $id;
+            $discussion->user_id = Auth::id();
+            $discussion->save();
+
+            return redirect("/task/$id");
+        }
+        else{
+            return redirect('/')->withErrors('You must be authenticated to post a new comment to a task.');
+        }
+    }
+
+    public function deleteChatMessage($task_id, $discussion_id){
+        if(Auth::check()){
+            Discussions::deleteDiscussion($discussion_id);
+            return redirect("/task/$task_id/edit");
+        }
+        else{
+            return redirect('/')->withErrors('You must be authenticated to delete a comment for a task.');
+        }
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -33,151 +33,23 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
-	Route::get('/', function () {
-        if(Auth::check()){
-            return view('tasks', [
-                'tasks' => Task::orderBy('created_at','asc')->get()
-            ]);
-        }
-        else{
-            return redirect('/login');
-        }
-
-	});
-
-	Route::post('/task',function (Request $request){
-
-	$validator = Validator::make($request->all(),[
-            'name' => 'required|max:255',
-            'priority' => 'required',
-            'description' => 'required|max:255',
-            'duedate' => 'required'
-		]);
-
-
-		if ($validator->fails()) {
-			return redirect('/')
-				->withInput()
-				->withErrors($validator);
-		}
-
-		$task = new Task;
-		$task->name = $request->name;
-        $task->description = $request->description;
-        $task->priority_id = $request->priority;
-		$task->due = $request->duedate;
-        $task->color_id = $request->color;
-        $task->assignee_id = $request->assignee;
-        $task->status_id = 0;//New task
-		$task->save();
-		return redirect('/');
-	});
-
-	Route::delete('/task/{id}',function ($id) {
-
-        if(!Auth::check()){
-            return redirect("/")->withErrors(['You need to be authenticated to delete a task.']);
-
-        }else {
-            Task::findOrFail($id)->delete();
-        }
-	return redirect('/');
-	});
-        
-        Route::get('/task/{id}', function($id){
-            $task = Task::findorfail($id);
-            $discussions = Discussions::getDiscussionsForPost($id);
-            $data = array(
-                'task' => $task,
-                'discussions' => $discussions,
-            );
-            return view('taskview', $data);
-        });
-
-        Route::get('/task/{id}/edit', function($id){
-            $task = Task::findorfail($id);
-            $discussions = Discussions::getDiscussionsForPost($id);
-            $users = User::orderBy('id', 'desc')->get();
-            $priorities = Priority::orderBy('id', 'asc')->get();
-            $colors = Color::orderBy('id', 'asc')->get();
-            $status = Status::orderBy('id', 'asc')->get();
-
-            $data = array(
-                'task' => $task,
-                'discussions' => $discussions,
-                'users' => $users,
-                'priorities' => $priorities,
-                'colors' => $colors,
-                'status' => $status
-            );
-
-            return view('taskedit', $data);
-        });
-
-        Route::post('/task/{id}/edit', function(Request $request, $id){
-            $task = Task::findorfail($id);
-            $task->name = $request->name;
-            $task->description = $request->description;
-            $task->assignee_id = $request->assignee;
-            $task->priority_id = $request->priority;
-            $task->due = $request->duedate;
-            $task->color_id = $request->color;
-            $task->status_id = $request->status;
-
-            $task->save();
-
-            return redirect("/task/$id");
-        });
-    
-        Route::delete('/task/{task_id}/deletechat/{discussion_id}', function($task_id, $discussion_id){
-            Discussions::deleteDiscussion($discussion_id);
-            return redirect("/task/$task_id/edit");
-        });
-        
-        Route::post('/task/{id}/postchat',function (Request $request, $id){
-            
-            if(!Auth::check()){
-                return redirect("/task/$id")->withErrors(['You need to be authenticated to post here.']);
-                
-            }
-
-            $validator = Validator::make($request->all(),[
-                'discussion' => 'required|min:1',
-            ]);
-            
-            if ($validator->fails()) {
-                    return redirect("/task/$id")
-                            ->withInput()
-                            ->withErrors($validator);
-            }
-            
-            $discussion = new Discussions;
-            $discussion->message = $request->discussion;
-            $discussion->posted_time = Carbon\Carbon::now();
-            $discussion->task_id = $id;
-            $discussion->user_id = Auth::id();
-            $discussion->save();
-
-            return redirect("/task/$id");
-	});
-});
-
-Route::group(['middleware' => 'web'], function () {
-    Route::auth();
+    Route::get('/', 'TaskController@getTasks');
 
     Route::get('/home', 'HomeController@index');
 
-    Route::get('/newtask', function() {
-        if(Auth::check()){
-            return view('newtask', [
-                'users' => User::orderBy('id', 'desc')->get(),
-                'priorities' => Priority::orderBy('id', 'asc')->get(),
-                'colors' => Color::orderBy('id', 'asc')->get()
-            ]);
-        }
-        else{
-            return redirect('/')->withErrors('You must be logged in to view that.');
-        }
-    });
+	Route::post('/task', 'TaskController@createTask');
+
+	Route::delete('/task/{id}', 'TaskController@deleteTask');
+
+    Route::get('/task/{id}', 'TaskController@getTask');
+
+    Route::get('/task/{id}/edit', 'TaskController@editTask');
+    
+    Route::post('/task/{id}/edit', 'TaskController@updateTask');
+    
+    Route::delete('/task/{task_id}/deletechat/{discussion_id}', 'DiscussionController@deleteChatMessage');
+    
+    Route::post('/task/{id}/postchat', 'DiscussionController@addChatMessage');
+
+    Route::get('/newtask', 'TaskController@createTaskForm');
 });
