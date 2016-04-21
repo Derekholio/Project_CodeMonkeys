688a0d7a9ae6c65049be6c55258f4b48246a051c
Derek Sims <support@derekholio.com>
2016-04-21 02:01:42 -0500

    Created new migration to rename tasks.status to tasks.status_id

---
+++ database/migrations/2016_04_21_015604_rename_status.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameStatus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function($table){
+            $table->renameColumn('status', 'status_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
